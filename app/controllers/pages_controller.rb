66b86bcf0dd40742da4baea9c8205b1178299447
joconnor <jonathan.oconnor@xcom.de>
2015-12-31 17:30:31 +0000

    Added home2 page for playing with new look for front page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,10 @@ class PagesController < ApplicationController
     @news = News.search({ active: "true" }, news_index_path, per_page: 20)
   end
 
+  def home2
+    @news = News.search({ active: "true" }, news_index_path, per_page: 20)
+  end
+
   def not_found
     render file: "#{Rails.root}/public/404", formats: [:html], layout: false, status: 404
   end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ IcuWwwApp::Application.routes.draw do
   get  "redirect" => "redirects#redirect"
   get  "header"   => "header#control"
 
-  %w[home links juniors].each do |page|
+  %w[home home2 links juniors].each do |page|
     get page => "pages##{page}"
   end
   %w[clubs events].each do |page|
