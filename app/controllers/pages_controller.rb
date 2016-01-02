a49114f90e9b71dd7b7e3f759da0384a9852c91f
joconnor <jonathan.oconnor@xcom.de>
2016-01-02 17:12:04 +0000

    Split the different pieces of content into different partials. Added headers for latest news.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,8 +5,8 @@ class PagesController < ApplicationController
 
   def home2
     @news = News.search({ active: "true" }, news_index_path, per_page: 5)
-    @juniors = Event.active.where(category: 'junior').where('start_date >= ?', Date.today).limit(3)
-    @irish = Event.active.where(category: 'irish').where('start_date >= ?', Date.today).limit(3)
+    @juniors = Event.active.where(category: 'junior').where('end_date >= ?', Date.today).limit(3)
+    @irish = Event.active.where(category: 'irish').where('end_date >= ?', Date.today).limit(3)
   end
 
   def not_found
