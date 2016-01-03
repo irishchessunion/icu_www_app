db277117cfa2b6fa2205313a2de51475fa5b40bf
joconnor <jonathan.oconnor@xcom.de>
2016-01-03 11:33:53 +0000

    Made final improvements in layout of home2 page, and migrated the home page over to new look trialed on home2

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,14 @@
 class PagesController < ApplicationController
   def home
-    @news = News.search({ active: "true" }, news_index_path, per_page: 20)
+    @news = News.search({ active: "true" }, news_index_path, per_page: 7)
+    @junior_events = Event.active.where(category: 'junior').where('end_date >= ?', Date.today).limit(3)
+    @irish_events = Event.active.where(category: 'irish').where('end_date >= ?', Date.today).limit(3)
   end
 
   def home2
-    @news = News.search({ active: "true" }, news_index_path, per_page: 5)
-    @juniors = Event.active.where(category: 'junior').where('end_date >= ?', Date.today).limit(3)
-    @irish = Event.active.where(category: 'irish').where('end_date >= ?', Date.today).limit(3)
+    @news = News.search({ active: "true" }, news_index_path, per_page: 7)
+    @junior_events = Event.active.where(category: 'junior').where('end_date >= ?', Date.today).limit(3)
+    @irish_events = Event.active.where(category: 'irish').where('end_date >= ?', Date.today).limit(3)
   end
 
   def not_found
