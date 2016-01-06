a309f9524ea66d6527a3bc12fabe147474753480
joconnor <jonathan.oconnor@xcom.de>
2016-01-06 11:17:27 +0000

    Bumped up number of irish events to 4 in home page list

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,13 +2,13 @@ class PagesController < ApplicationController
   def home
     @news = News.search({ active: "true" }, news_index_path, per_page: 7)
     @junior_events = Event.active.where(category: 'junior').where('end_date >= ?', Date.today).limit(3)
-    @irish_events = Event.active.where(category: 'irish').where('end_date >= ?', Date.today).limit(3)
+    @irish_events = Event.active.where(category: 'irish').where('end_date >= ?', Date.today).limit(4)
   end
 
   def home2
     @news = News.search({ active: "true" }, news_index_path, per_page: 7)
     @junior_events = Event.active.where(category: 'junior').where('end_date >= ?', Date.today).limit(3)
-    @irish_events = Event.active.where(category: 'irish').where('end_date >= ?', Date.today).limit(3)
+    @irish_events = Event.active.where(category: 'irish').where('end_date >= ?', Date.today).limit(4)
   end
 
   def not_found
