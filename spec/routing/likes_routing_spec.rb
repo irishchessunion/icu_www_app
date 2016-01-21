e345962093c9c84c115e062e48cc1bfc37966e76
joconnor <jonathan.oconnor@xcom.de>
2016-01-21 15:55:10 +0000

    Fixed routing spec for LikesController

--- spec/routing/likes_routing_spec.rb
+++ spec/routing/likes_routing_spec.rb
@@ -1,14 +1,14 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe LikesController, type: :routing do
-  describe "routing" do
+  describe 'routing' do
 
-    it "routes to #create" do
-      expect(:post => "/likes?news_id=2").to route_to("likes#create")
+    it 'routes to #create' do
+      expect(:post => '/likes?news_id=2').to route_to('likes#create', news_id: '2')
     end
 
-    it "routes to #destroy" do
-      expect(:delete => "/likes/1").to route_to("likes#destroy", :id => "1")
+    it 'routes to #destroy' do
+      expect(:delete => '/likes/1').to route_to('likes#destroy', id: '1')
     end
 
   end
