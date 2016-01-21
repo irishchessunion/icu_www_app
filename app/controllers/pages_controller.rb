77dc131b0de35edc95ff2db7a302b2d5f76d5535
joconnor <jonathan.oconnor@xcom.de>
2016-01-21 10:15:53 +0000

    Bumped the number of news items shown from 7 to 8.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,12 @@
 class PagesController < ApplicationController
   def home
-    @news = News.search({ active: "true" }, news_index_path, per_page: 7)
+    @news = News.search({ active: "true" }, news_index_path, per_page: 8)
     @junior_events = Event.active.where(category: 'junior').where('end_date >= ?', Date.today).limit(3)
     @irish_events = Event.active.where(category: %w(irish women)).where('end_date >= ?', Date.today).limit(4)
   end
 
   def home2
-    @news = News.search({ active: "true" }, news_index_path, per_page: 7)
+    @news = News.search({ active: "true" }, news_index_path, per_page: 8)
     @junior_events = Event.active.where(category: 'junior').where('end_date >= ?', Date.today).limit(3)
     @irish_events = Event.active.where(category: %w(irish women)).where('end_date >= ?', Date.today).limit(4)
   end
