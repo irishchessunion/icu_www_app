9f1f7026492eb90538dc6ab78895d9e216cbbd7e
joconnor <jonathan.oconnor@xcom.de>
2016-02-23 12:55:05 +0000

    Fixed bug in result reporting which didn't allow normal members to report results. Added tests of Ability to ensure it does work.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,61 +4,59 @@ class Ability
   def initialize(user)
     return if user.guest?
 
-    if user.roles.present?
-      if user.admin?
-        can :manage, :all
-        return
-      end
+    if user.admin?
+      can :manage, :all
+      return
+    end
 
-      if user.tester?
-        can :manage, Admin::Statistic
-      end
+    if user.tester?
+      can :manage, Admin::Statistic
+    end
 
-      if user.editor?
-        can :create, [Article, Event, Image, News]
-        can [:create, :show], Download
-        can [:create, :index, :show], Pgn
-        can [:destroy, :update], [Article, Event, Image, News, Pgn, Download], user_id: user.id
-        can [:destroy, :update], Game, pgn: { user_id: user.id }
-        can :manage, [Champion, Club, Series, Tournament]
-      end
+    if user.editor?
+      can :create, [Article, Event, Image, News]
+      can [:create, :show], Download
+      can [:create, :index, :show], Pgn
+      can [:destroy, :update], [Article, Event, Image, News, Pgn, Download], user_id: user.id
+      can [:destroy, :update], Game, pgn: { user_id: user.id }
+      can :manage, [Champion, Club, Series, Tournament]
+    end
 
-      if user.calendar?
-        can [:create, :update], Event
-        can :destroy, Event, user_id: user.id
-      end
+    if user.calendar?
+      can [:create, :update], Event
+      can :destroy, Event, user_id: user.id
+    end
 
-      if user.membership?
-        can :create, CashPayment
-        can :manage, Player
-      end
+    if user.membership?
+      can :create, CashPayment
+      can :manage, Player
+    end
 
-      if user.inspector?
-        can :show, Player
-      end
+    if user.inspector?
+      can :show, Player
+    end
 
-      # Most people can report results
-      # If you created a result, you can delete it.
-      # Reporters can delete messages or ban people from reporting.
-      unless user.disallow_reporting?
-        can :create, Result
-      end
-      can :destroy, Result, reporter_id: user.id
+    # Most people can report results
+    # If you created a result, you can delete it.
+    # Reporters can delete messages or ban people from reporting.
+    unless user.disallow_reporting?
+      can :create, Result
+    end
+    can :destroy, Result, reporter_id: user.id
 
-      if user.reporter?
-        can [:destroy, :update], Result
-      end
+    if user.reporter?
+      can [:destroy, :update], Result
+    end
 
-      if user.translator?
-        can :manage, Translation
-        can :show, JournalEntry, journalable_type: "Translation"
-      end
+    if user.translator?
+      can :manage, Translation
+      can :show, JournalEntry, journalable_type: "Translation"
+    end
 
-      if user.treasurer?
-        can :create, CashPayment
-        can :index, [Item, PaymentError, Refund]
-        can :manage, [Cart, Fee, UserInput]
-      end
+    if user.treasurer?
+      can :create, CashPayment
+      can :index, [Item, PaymentError, Refund]
+      can :manage, [Cart, Fee, UserInput]
     end
 
     can :manage_preferences, User, id: user.id
--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -15,4 +15,16 @@ describe Ability do
       end
     end
   end
+
+  context User do
+
+    it "can report results" do
+      expect(Ability.new(User.new).can?(:create, Result)).to be true
+    end
+
+    it "can't report results if reporting disallowed" do
+      expect(Ability.new(User.new(disallow_reporting: true)).can?(:create, Result)).to be false
+    end
+
+  end
 end
