db7961b4c10913656824ef996b7c6956600066a9
murrayben <dayintoknight@gmail.com>
2023-09-07 17:57:03 +0100

    payment errors are now in desc order

--- app/models/payment_error.rb
+++ app/models/payment_error.rb
@@ -2,10 +2,10 @@ class PaymentError < ApplicationRecord
   include Pageable
 
   belongs_to :cart
-  default_scope { order(:created_at) }
+  scope :ordered, -> { order(created_at: :desc) }
 
   def self.search(params, path)
-    matches = all
+    matches = ordered
     %w[message details payment_name confirmation_email created_at].each do |property|
       matches = matches.where("#{property} LIKE ?", "%#{params[property.to_sym]}%") if params[property.to_sym].present?
     end
