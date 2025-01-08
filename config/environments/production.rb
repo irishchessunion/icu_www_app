3563cec692b6c840368e6f71e25f711fa02d7ea2
murrayben <dayintoknight@gmail.com>
2025-01-08 18:40:13 +0000

    updated production log file level to info

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ IcuWwwApp::Application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :debug
+  config.log_level = :info
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
